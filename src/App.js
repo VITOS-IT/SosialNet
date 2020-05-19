40448131e9116c9f73bcd5bdd58f06c7162c27d3
VITOS-IT <vitos.iva@gmail.com>
2020-05-19 20:45:10 +0300

    32 lesson

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ const App = (props) => {
                 <Navbar state={props.state.navBlock}/>
                 {/*<Profile/>*/}
                 <div className={'app-wrapper-content'}>
-                    <Route path={'/profile'} render={()=><Profile state={props.state.profilePage}/>}/>
+                    <Route path={'/profile'} render={()=><Profile state={props.state.profilePage}
+                                                        addPost={props.addPost}/>}/>
                     <Route path='/dialogs' render={() => <Dialogs state={props.state.messagesPage}/>}/>
                     <Route path={'/news'} component={News}/>
                 </div>
--- src/components/Dialogs/Dialods.jsx
+++ src/components/Dialogs/Dialods.jsx
@@ -4,17 +4,30 @@ import DialogItem from "./DialogItem/DialodItem";
 import Message from "./Message/Message";
 
 
-const Dialogs = (props) =>{
-    let messagesElements = props.state.messageData.map(mes => <Message message = {mes.message}/>);
-    let element = props.state.dialogsData.map(d => <DialogItem name ={d.name} id = {d.id}
-                                                                avatar={d.avatar}/> );
-    return(
-        <div className={s.dialogs}>
-            <div className={s.dialogsItem}>
-                {element}
-            </div>
-            <div className={s.messages}>
-                {messagesElements}
+const Dialogs = (props) => {
+    let messagesElements = props.state.messageData.map(mes => <Message message={mes.message}/>);
+    let element = props.state.dialogsData.map(d => <DialogItem name={d.name} id={d.id}
+                                                               avatar={d.avatar}/>);
+    let newMessage = React.createRef();
+    let addMessage = () =>{
+        let text = newMessage.current.value;
+        alert(text);
+    }
+
+    return (
+        <div>
+            <div className={s.dialogs}>
+                <div className={s.dialogsItem}>
+                    {element}
+                </div>
+                <div className={s.messages}>
+                    {messagesElements}
+                    <div><textarea ref={newMessage}></textarea></div>
+                    <div>
+                        <button onClick={addMessage}>Send message</button>
+                    </div>
+                </div>
+
             </div>
 
         </div>
