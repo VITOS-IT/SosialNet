bb2fad88d2167645c3f045d7ddcdb98329b7ae16
VITOS-IT <vitos.iva@gmail.com>
2020-05-25 08:13:43 +0300

    35 lesson

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import {rerenderEntireTree} from "./render";
-import state from "./redux/state";
+import state, {addPost, subscribe, updateNewPostText} from "./redux/state";
+import ReactDOM from "react-dom";
+import {BrowserRouter} from "react-router-dom";
+import App from "./App";
 
 
+
+
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+        </BrowserRouter>, document.getElementById('root')
+    );
+}
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,65 @@
-import {rerenderEntireTree} from "../render";
+// let store = {
+//     _state: {
+//         profilePage: {
+//             postsData: [
+//                 {id: 1, message: 'Hi, how are you?', likesCount: 15},
+//                 {id: 2, message: 'Hi, how are youssss?', likesCount: 20}],
+//             newPostText: 'it-kamasutra.com'
+//         },
+//         messagesPage: {
+//             messageData: [
+//                 {id: 1, message: 'Hi'},
+//                 {id: 2, message: 'How are you'},
+//                 {id: 3, message: 'yo'},
+//                 {id: 4, message: 'yo'},
+//                 {id: 5, message: 'yo'}],
+//             dialogsData: [
+//                 {id: 1, name: 'Dimych', avatar: 'https://download-cs.net/steam/avatars/3412.jpg'},
+//                 {id: 2, name: 'Andrew', avatar: 'https://download-cs.net/steam/avatars/3412.jpg'},
+//                 {id: 3, name: 'Sweta', avatar: 'https://download-cs.net/steam/avatars/3412.jpg'},
+//                 {id: 4, name: 'Anna', avatar: 'https://download-cs.net/steam/avatars/3412.jpg'},
+//                 {id: 5, name: 'Den', avatar: 'https://download-cs.net/steam/avatars/3412.jpg'}]
+//         },
+//         navBlock: {
+//             friendItem: [
+//                 {id: 1, name: 'Dimych', avatar: 'https://download-cs.net/steam/avatars/3412.jpg'},
+//                 {id: 2, name: 'Andrew', avatar: 'https://download-cs.net/steam/avatars/3412.jpg'},
+//                 {id: 3, name: 'Sweta', avatar: 'https://download-cs.net/steam/avatars/3412.jpg'}
+//             ]
+//         }
+//     },
+//
+//     getState() {
+//         return this._state;
+//     },
+//     _callSubscriber(){
+//         console.log('aasaasa')
+//     },
+//
+//     addPost() {
+//         let newPost = {
+//             id: 5,
+//             message: this._state.profilePage.newPostText,
+//             likesCount: 0
+//         }
+//         this._state.profilePage.postsData.push(newPost);
+//         this._state.profilePage.newPostText = '';
+//         this._callSubscriber();
+//     },
+//     updateNewPostText(newText) {
+//         this._state.profilePage.newPostText = newText;
+//         this._callSubscriber();
+//     },
+//     subscribe(observer){
+//         this._callSubscriber = observer;
+//     },
+//
+//
+//
+// }
+// export default store;
+
+
 
 let state = {
     profilePage:{
@@ -32,7 +93,10 @@ let state = {
 
 };
 
-export let addPost=()=>{
+let rerenderEntireTree = () =>{
+    console.log('setate chaned');
+}
+export const addPost=()=>{
     let newPost={
         id:5,
         message:state.profilePage.newPostText,
@@ -43,8 +107,12 @@ export let addPost=()=>{
     rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText)=>{
+export const updateNewPostText = (newText)=>{
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
 }
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
+}
+
 export default state
