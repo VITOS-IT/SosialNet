e42da2a5940461c2b08e6d19e60c36204f38269c
VITOS-IT <vitos.iva@gmail.com>
2020-06-14 22:03:10 +0300

    55 lesson pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,21 +4,43 @@ import * as axios from "axios";
 import userPhoto from './../../assets/images/user.png'
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
-        if (this.props.users.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
 
+    componentDidMount() {
+        if (this.props.users.length === 0) {
+            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
                 this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount)
             });
         }
     }
+    onPageChanget = (pageNumber)=>{
+        this.props.setCurrentPage(pageNumber)
+            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+                this.props.setUsers(response.data.items);
+            });
+
+    }
+
     render() {
-       return <div>
-        {
-            this.props.users.map(u => <div key={u.id} className={styles.userItem}>
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+        return <div>
+            <div>
+                {pages.map(p => {
+                  return <span className={this.props.currentPage === p && styles.selectedPage}
+                  onClick={(e)=> {this.onPageChanget(p)}}> {p}</span>
+                })
+                }
+
+            </div>
+            {
+                this.props.users.map(u => <div key={u.id} className={styles.userItem}>
                 <span>
-                    <div><img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/></div>
+                    <div><img src={u.photos.small != null ? u.photos.small : userPhoto}
+                              className={styles.userPhoto}/></div>
                     <div>
                         {u.followed ?
                             <button onClick={() => {
@@ -29,7 +51,7 @@ class Users extends React.Component {
                             }}>Follow</button>}
                     </div>
                 </span>
-                <span>
+                    <span>
                     <span>
                         <div>{u.name}</div>
                         <div>{u.status}</div>
@@ -39,11 +61,11 @@ class Users extends React.Component {
                         <div>{'u.location.sity'}</div>
                     </span>
                 </span>
-                <br/>
-            </div>)
-        }
-    </div>
-        
+                    <br/>
+                </div>)
+            }
+        </div>
+
     }
 }
 
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Users from "./Users";
 import {connect} from "react-redux";
-import {followAC, setUsersAC, unFollowAC} from "../../redux/usersReduser";
+import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unFollowAC} from "../../redux/usersReduser";
 
 
 let mapStateToProps = (state) =>{
     return{
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 }
 let mapDispatchToProps = (dispatch) =>{
@@ -21,6 +24,12 @@ let mapDispatchToProps = (dispatch) =>{
         },
         setUsers: (users) =>{
             dispatch(setUsersAC(users))
+        },
+        setCurrentPage: (pageNumber) =>{
+            dispatch(setCurrentPageAC(pageNumber))
+        },
+        setTotalUsersCount: (totalCount) =>{
+            dispatch(setTotalUsersCountAC(totalCount))
         }
     }
 }
